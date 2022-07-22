c79db1719108498df0125f3bb52318b954853ddb
Aleksandar Tošić <tosaleks@gmail.com>
2022-07-22 12:07:19 +0200

    transaction data

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,2 +1,26 @@
 public class Transaction {
+    String type;
+    String hash;
+    int nonce;
+    int transaction_index;
+    String from_address;
+    String to_address;
+    Long value;
+    long gas;
+    long gas_price;
+    String input;
+    long block_timestamp;
+    long block_number;
+    String block_hash;
+    long max_fee_per_gas;
+    long max_priority_fee_per_gas;
+    int transaction_type;
+    long receipt_cumulative_gas_used;
+    long receipt_gas_used;
+    String receipt_contract_address;
+    String receipt_root;
+    int receipt_status;
+    long receipt_effective_gas_price;
+    int item_id;
+    long item_timestamp;
 }
