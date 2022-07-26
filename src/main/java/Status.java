890c20e472b0779dece0447faf1ed81c0a2ca090
Aleksandar Tošić <tosaleks@gmail.com>
2022-07-26 09:49:36 +0200

    working version

--- src/main/java/Status.java
+++ src/main/java/Status.java
@@ -8,9 +8,10 @@ public class Status extends TimerTask {
     private int ticks;
     private int newVertices;
     private int totalVertices;
+    private int currentBlock;
+    private Config config;
 
-
-    public Status() {
+    public Status(Config config) {
         totalTrasactions = 0;
         currentTransactions = 0;
         queueSize = 0;
@@ -18,16 +19,22 @@ public class Status extends TimerTask {
         newVertices = 0;
         totalVertices = 0;
         ticks = 1;
+        currentBlock = 1;
+        this.config = config;
     }
 
     @Override
     public void run() {
+        float progress = ((currentBlock *1f / (config.endBlock - config.startBlock) * 100));
         System.out.print(Constants.STATUS + "Processed: " + currentTransactions
                 + " Total: " + totalTrasactions
                 + " Avg " + (totalTrasactions / ticks) + " /s"
                 + " Vertices new: " + newVertices
                 + " Vertices total: " + totalVertices
                 + " Level: " + level
+                + " CurrentBLock: " + currentBlock
+                + " Progress: " + Math.round(progress) + " %"
+                + " QueueSize: " + queueSize
                 + " \r");
         reset();
     }
@@ -46,5 +53,13 @@ public class Status extends TimerTask {
 
     public synchronized void newVertex() {
         newVertices++;
+    }
+
+    public synchronized void newBlock() {
+        currentBlock++;
+    }
+
+    public void resetCurrentBlock() {
+        currentBlock = 1;
     }
 }
